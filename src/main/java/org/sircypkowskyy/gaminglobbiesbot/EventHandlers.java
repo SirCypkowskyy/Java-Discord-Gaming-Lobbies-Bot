d5f1e05f5d8a3b9bacb4d5d38ad1881325253f80
SirCypkowskyy <dcyprian.a.gburek@gmail.com>
2022-11-15 02:46:52 +0100

    Host leaves lobby fix
    Lobbies won't be deleted when the host leaves the VC
    
    Lobbies now get deleted only if VC is empty for more than one minute from the time of the lobby creation AND if the lobby's host clicks "Leave lobby" on the lobby announcement message button

--- src/main/java/org/sircypkowskyy/gaminglobbiesbot/EventHandlers.java
+++ src/main/java/org/sircypkowskyy/gaminglobbiesbot/EventHandlers.java
@@ -12,24 +12,27 @@ public class EventHandlers extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
-        List<Document> lobbies = Main.dataManager.getLobbies().find().into(new ArrayList<>());
-        var lobby = lobbies.stream().filter(x -> x.getLong("lobbyChannelId") == event.getChannelLeft().getIdLong()).findFirst();
+        // Commented for now
+        // Lobbies shouldn't be deleted when the host leaves due to the possibility of the host unexpectedly leaving
 
-        if(lobby.isEmpty())
-            return;
-
-        if(event.getMember().getIdLong() == lobby.get().getLong("lobbyUserOwnerId")) {
-            try {
-                event.getGuild().getTextChannelById(lobby.get().getLong("lobbyInfoMessageChannelId")).retrieveMessageById(lobby.get().getLong("lobbyInfoMessageId")).queue(x -> {
-                    x.delete().queue();
-                });
-            }
-            catch (Exception ignored) {
-                System.out.println("Info message not found!");
-            }
-            Main.dataManager.getLobbies().deleteOne(lobby.get());
-            if(event.getGuild().getVoiceChannelById(lobby.get().getLong("lobbyChannelId")) != null)
-                event.getGuild().getVoiceChannelById(lobby.get().getLong("lobbyChannelId")).delete().queue();
-        }
+//        List<Document> lobbies = Main.dataManager.getLobbies().find().into(new ArrayList<>());
+//        var lobby = lobbies.stream().filter(x -> x.getLong("lobbyChannelId") == event.getChannelLeft().getIdLong()).findFirst();
+//
+//        if(lobby.isEmpty())
+//            return;
+//
+//        if(event.getMember().getIdLong() == lobby.get().getLong("lobbyUserOwnerId")) {
+//            try {
+//                event.getGuild().getTextChannelById(lobby.get().getLong("lobbyInfoMessageChannelId")).retrieveMessageById(lobby.get().getLong("lobbyInfoMessageId")).queue(x -> {
+//                    x.delete().queue();
+//                });
+//            }
+//            catch (Exception ignored) {
+//                System.out.println("Info message not found!");
+//            }
+//            Main.dataManager.getLobbies().deleteOne(lobby.get());
+//            if(event.getGuild().getVoiceChannelById(lobby.get().getLong("lobbyChannelId")) != null)
+//                event.getGuild().getVoiceChannelById(lobby.get().getLong("lobbyChannelId")).delete().queue();
+//        }
     }
 }
